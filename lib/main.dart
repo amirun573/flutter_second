cf63f3c0f504777680446835e8343e1afbac29ff
amirun573 <amirunaqmar@gmail.com>
2021-01-03 21:14:13 +0800

    Use Expanded for more flexible according to screen

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,17 @@ class DicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Image(
-          image: AssetImage('images/dice1.png'),
+        Expanded(
+          flex: 2,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
         ),
       ],
     );
