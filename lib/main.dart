c505f8d99582540c45f4d194e7845de663c95c3c
amirun573 <amirunaqmar@gmail.com>
2021-01-03 21:24:37 +0800

    Use Constructir For Image Property

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,9 @@ class DicePage extends StatelessWidget {
     return Row(
       children: <Widget>[
         Expanded(
-          flex: 2,
-          child: Image(
-            image: AssetImage('images/dice1.png'),
-          ),
+          child: Image.asset('images/dice1.png'),
         ),
-        Expanded(
-          flex: 1,
-          child: Image(
-            image: AssetImage('images/dice1.png'),
-          ),
-        ),
+        Expanded(child: Image.asset('images/dice2.png')),
       ],
     );
   }
